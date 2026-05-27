cd6f7b07ef375a41132c0565881e7e7e4f5a0b3d
abu-ubaitha <muhammed.abu.ubaitha@gmail.com>
2026-05-27 19:47:55 +0530

    fixed LB 01

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
+
 const supabaseUrl =
-"https://supabase.com/dashboard/project/isgigweowcrlzcqaaywv/settings/api-keys";
+"https://isgigweowcrlzcqaaywv.supabase.co";
 
 const supabaseKey =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZ2lnd2Vvd2NybHpjcWFheXd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NDM2MjMsImV4cCI6MjA5NTIxOTYyM30.KCcPw5EW2_w62LDqUreoPr5akiEFFTKkKuHyDW9sL-0";
